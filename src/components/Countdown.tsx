9a8b0c900426dcfea898f8300dc3669efabd6693
SadVitorGomez <7vitorgomez@gmail.com>
2021-02-27 12:42:07 -0300

    Correction: SVG hover fill

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -32,9 +32,8 @@ export function Countdown() {
 
       {hasFinished ? (
         <button disabled className={styles.countdownButton}>
-          <p>
-            Ciclo encerrado <img src="/icons/check.svg" alt="Check" />
-          </p>
+          Ciclo encerrado
+          <img src="/icons/check.svg" alt="Check" />
         </button>
       ) : (
         <>
@@ -44,9 +43,16 @@ export function Countdown() {
               onClick={resetCountdown}
               className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
             >
-              <p>
-                Abandonar ciclo <img src="/icons/stop.svg" alt="stop" />
-              </p>
+              Abandonar ciclo
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                fill="#666666"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z" />
+              </svg>
             </button>
           ) : (
             <button
